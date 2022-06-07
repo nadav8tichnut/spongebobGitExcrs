ed4a2a8968c40cefb9eafb348f67d509e63d3a9f
liordavidlevy <liordavid20@gmail.com>
2022-06-07 16:10:42 +0300

    added secret ingredient

---
+++ Hamburger.java
@@ -0,0 +1,32 @@
+public class Hamburger {
+    public static void main(String[] args) {
+        SpiceMeat();
+        FryHamburger();
+        PutInBun();
+        AddVegetables();
+    }
+
+    public static void SpiceMeat()
+    {
+        System.out.println("Spicing with some garlic and paprica");
+    }
+
+    public static void FryHamburger()
+    {
+        System.out.println("Frying the 100% olive oil");
+    }
+
+    public static void PutInBun()
+    {
+        System.out.println("Insert the hamburger to the fresh hot bun");
+    }
+
+    public static void AddVegetables()
+    {
+        System.out.println("Add some live vegetables");
+    }
+
+//    the secret ingredient is: "PLANKTONS!!!"
+
+}
+
